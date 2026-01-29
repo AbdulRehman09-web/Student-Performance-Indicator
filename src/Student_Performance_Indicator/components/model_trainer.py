2fe340535e3df0c155231b96ee86757928dabd0d
Abdul Rehman <abdulrehmannadeem825@gmail.com>
2026-01-29 23:13:28 +0500

    Model Monitoring

--- src/Student_Performance_Indicator/components/model_trainer.py
+++ src/Student_Performance_Indicator/components/model_trainer.py
@@ -5,30 +5,41 @@ import os
 import sys
 import mlflow
 import mlflow.sklearn
-from urllib.parse import urlparse
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 # from catboost import CatBoostRegressor
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from src.Student_Performance_Indicator.utils import save_object, evaluate_models
+import numpy as np
 
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
+
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self, actual, pred):
+        """Compute RMSE, MAE, R2"""
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Model Trainer initiated")
 
+            # Split input arrays
             logging.info("Splitting training and testing input data")
-            X_train, y_train = train_array[:,:-1], train_array[:,-1]
-            X_test, y_test = test_array[:,:-1], test_array[:,-1]
+            X_train, y_train = train_array[:, :-1], train_array[:, -1]
+            X_test, y_test = test_array[:, :-1], test_array[:, -1]
 
+            # Define models
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -37,102 +48,64 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(),
                 # "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
-                }
-            params = {
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                },
-                "Random Forest": {
-                    'n_estimators':[8,16,32,64,128,256]
-                },
-                "Gradient Boosting": {
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    'n_estimators':[8,16,32,64,128,256]
-                },
-                "Linear Regression": {},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators':[8,16,32,64,128,256]
-                },
-                # "CatBoosting Regressor":{
-                #     'depth':[6,8,10],
-                #     'learning_rate':[.1,.01,.05,.001],
-                #     'iterations':[30,50,100]
-                # },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators':[8,16,32,64,128,256]
-                }
             }
-            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params)
 
-            ## To get best model score from dict
+            # Define hyperparameters
+            params = {
+                "Decision Tree": {'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']},
+                "Random Forest": {'n_estimators': [8, 16, 32, 64, 128, 256]},
+                "Gradient Boosting": {'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                                      'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                                      'n_estimators': [8, 16, 32, 64, 128, 256]},
+                "Linear Regression": {},
+                "XGBRegressor": {'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                                 'n_estimators': [8, 16, 32, 64, 128, 256]},
+                # "CatBoosting Regressor": {'depth': [6, 8, 10], 'learning_rate': [0.1, 0.01, 0.05, 0.001], 'iterations': [30, 50, 100]},
+                "AdaBoost Regressor": {'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                                       'n_estimators': [8, 16, 32, 64, 128, 256]}
+            }
+
+            # Evaluate models and get scores
+            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+
+            # Best model selection
             best_model_score = max(sorted(model_report.values()))
-
-             ## To get best model name from dict
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
 
-            print("This is the best model:")
-            print(best_model_name)
+            print(f"This is the best model: {best_model_name}")
 
-            model_names = list(params.keys())
+            # Get best parameters
+            best_params = params[best_model_name]
 
-            actual_model=""
+            # ---------------- MLflow Integration ----------------
+            # Windows-safe local tracking
+            mlflow.set_tracking_uri("mlruns")
+            experiment_name = "Student_Performance_Experiment"
+            mlflow.set_experiment(experiment_name)
 
-            for model in model_names:
-                if best_model_name == model:
-                    actual_model = actual_model + model
+            with mlflow.start_run():
+                predicted_qualities = best_model.predict(X_test)
+                rmse, mae, r2 = self.eval_metrics(y_test, predicted_qualities)
 
-            best_params = params[actual_model]
+                mlflow.log_params(best_params)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("mae", mae)
+                mlflow.log_metric("r2", r2)
 
-            # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/mlprojecthindi.mlflow")
-            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+                # Log model (no registry to avoid remote issues)
+                mlflow.sklearn.log_model(best_model, "model")
 
-            # # mlflow
+            print(f"Best Model Found: {best_model_name} with R2 Score: {best_model_score}")
 
-            # with mlflow.start_run():
+            if best_model_score < 0.6:
+                raise CustomException("No model found with sufficient R2 score!")
 
-            #     predicted_qualities = best_model.predict(X_test)
+            logging.info("Saving best model locally")
+            save_object(file_path=self.model_trainer_config.trained_model_file_path, obj=best_model)
 
-            #     (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+            return r2_score(y_test, best_model.predict(X_test))
 
-            #     mlflow.log_params(best_params)
-
-            #     mlflow.log_metric("rmse", rmse)
-            #     mlflow.log_metric("r2", r2)
-            #     mlflow.log_metric("mae", mae)
-
-
-            #     # Model registry does not work with file store
-            #     if tracking_url_type_store != "file":
-
-            #         # Register the model
-            #         # There are other ways to use the Model Registry, which depends on the use case,
-            #         # please refer to the doc for more information:
-            #         # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            #         mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
-            #     else:
-            #         mlflow.sklearn.log_model(best_model, "model")
-            print(f"Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}")
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
-
-            save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
-
-            predicted=best_model.predict(X_test)
-
-            r2_square = r2_score(y_test, predicted)
-            return r2_square
-            
         except Exception as e:
             logging.error("Error in Model Trainer")
             raise CustomException(e, sys)
